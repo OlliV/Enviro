9c3e90483c617079bf5d50cd91d4220753a576e5
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-02-01 18:17:37 +0200

    Cleanup the adal code

--- device/adal.js
+++ device/adal.js
@@ -1,57 +1,53 @@
 const fs = require('fs');
+const { promisify } = require('util');
 const adal = require('adal-node');
 
 const AuthenticationContext = adal.AuthenticationContext;
+const config = readConfig();
+
+function readConfig() {
+	const data = fs.readFileSync('./config.json');
+
+	if (data) {
+		return JSON.parse(data);
+	} else {
+		throw new Error(`JSON parsing error: ${parametersFile}`);
+	}
+}
 
 function turnOnLogging() {
 	const log = adal.Logging;
-	log.setLoggingOptions( {
+
+	log.setLoggingOptions({
 		level: log.LOGGING_LEVEL.VERBOSE,
 		log: (level, message, error) => {
 			console.log(message);
 			if (error) {
 				console.log(error);
 			}
-    }
-  });
+		}
+	});
 }
 
-function readConfig() {
-	const data = fs.readFileSync('./config.json');
+function createContext() {
+	const authorityUrl = `${config.authorityHostUrl}/${config.tenant}`;
 
-	if (data) {
-	  return JSON.parse(data);
-	} else {
-		throw new Error(`File not found: ${parametersFile}`);
-	}
+	turnOnLogging();
+
+	const tokenCache = new adal.MemoryCache();
+	const context = new AuthenticationContext(authorityUrl, true, tokenCache);
+
+	return context;
 }
 
-const config = readConfig();
-const authorityUrl = `${config.authorityHostUrl}/${config.tenant}`;
-
-turnOnLogging();
-
-const tokenCache = new adal.MemoryCache();
-const context = new AuthenticationContext(authorityUrl, true, tokenCache);
+const context = createContext();
+const acquireUserCode = promisify(context.acquireUserCode).bind(context);
+const acquireTokenWithDeviceCode = promisify(context.acquireTokenWithDeviceCode).bind(context);
 
 module.exports = async function acquireToken(resource) {
-	const userCodeInfo = await new Promise((resolve, reject) => context.acquireUserCode(resource, config.clientId, 'en-us', (err, res) => {
-		if (err) {
-			reject(err);
-		} else {
-			resolve(res);
-		}
-	}));
+	const userCodeInfo = await acquireUserCode(resource, config.clientId, 'en-us');
 
 	console.log('Use a web browser to open the page ' + userCodeInfo.verificationUrl + ' and enter the code ' + userCodeInfo.userCode + ' to sign in.');
 
-	return new Promise((resolve, reject) => {
-		context.acquireTokenWithDeviceCode(resource, config.clientId, userCodeInfo,  (err, tokenResponse) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(tokenResponse);
-			}
-		});
-	});
+	return acquireTokenWithDeviceCode(resource, config.clientId, userCodeInfo);
 };
